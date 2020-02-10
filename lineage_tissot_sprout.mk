6ba87df7787571b7219c640ba0573b5d8acc47ed
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 20:24:06 +0000

    tissot-user-9-PKQ1.180917.001-V10.0.17.0.PDHMIXM-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_tissot_sprout.mk
+++ lineage_tissot_sprout.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Mi A1
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tissot_sprout
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="tissot-user 9 PKQ1.180917.001 V10.0.12.0.PDHMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="tissot-user 9 PKQ1.180917.001 V10.0.17.0.PDHMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.12.0.PDHMIXM:user/release-keys
+BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.17.0.PDHMIXM:user/release-keys
